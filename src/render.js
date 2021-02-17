0b14f59635aa29bb9c671e5846f945a78952fa06
Nanjo <48948888+IamNanjo@users.noreply.github.com>
2021-02-17 10:14:14 +0200

    Add files via upload

---
+++ src/render.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const {getCurrentWindow} = require('electron').remote;
+const path = require("path")
+
+console.log(path.join(__dirname, "..", "Kuvat"));
+let folderContents = fs.readdirSync(path.join(__dirname, "..", "Kuvat"))
+folderContents.pop()
+console.log("Folder Contents", folderContents);
+switch (folderContents.length) {
+    case 0:
+        window.alert("Kuvia ei löytynyt")
+        break
+    case 1:
+        console.log("Case 1")
+        document.getElementById("files1").style.display = "grid"
+        document.getElementById("files2").style.display = "none"
+        document.getElementById("files3").style.display = "none"
+        document.getElementById("files4").style.display = "none"
+        break
+    
+    case 2:
+        console.log("Case 2");
+        document.getElementById("files1").style.display = "none"
+        document.getElementById("files2").style.display = "grid"
+        document.getElementById("files3").style.display = "none"
+        document.getElementById("files4").style.display = "none"
+        break
+    
+    case 3:
+        console.log("Case 3");
+        document.getElementById("files1").style.display = "none"
+        document.getElementById("files2").style.display = "none"
+        document.getElementById("files3").style.display = "grid"
+        document.getElementById("files4").style.display = "none"
+        break
+    
+    case 4:
+        console.log("Case 4");
+        document.getElementById("files1").style.display = "none"
+        document.getElementById("files2").style.display = "none"
+        document.getElementById("files3").style.display = "none"
+        document.getElementById("files4").style.display = "grid"
+        break
+
+    default:
+        if(folderContents.length > 4) {
+            document.getElementById("tooManyFiles").style.display = "block"
+            document.getElementById("files1").style.display = "none"
+            document.getElementById("files2").style.display = "none"
+            document.getElementById("files3").style.display = "none"
+            document.getElementById("files4").style.display = "grid"
+            setTimeout(() => document.getElementById("tooManyFiles").style.display = "none", 20000)
+        }
+        break
+}
+
+(() => { // Check file changes
+    fs.watch(path.join(__dirname, "..", "Kuvat"), (eventType, filename) => {
+        if(filename) {
+            switch(eventType) {
+                case "rename":
+                case "change":
+                    getCurrentWindow().reload()
+                    break
+            }
+        } else console.log("Filename not provided")
+    })
+    ()
+})()
